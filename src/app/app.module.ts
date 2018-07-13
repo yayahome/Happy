c68a863eded897471a106cab7d32ba9efb551454
Liuya <liuypersonalemail@163.com>
2018-07-13 19:43:58 +0800

    fff:Please enter the commit message for your changes. Lines starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderModule } from 'src/app/core/component/header/header';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
